fa27454871a8c05974cfdd640561ae413db998d4
Anky Y <baitoyawanis@gmail.com>
2021-01-17 02:30:42 +1100

    main

---
+++ RPGMakerActorGenerator/ActorForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace RPGMakerActorGenerator
+{
+    partial class ActorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ActorForm));
+            this.SourceDirLabel = new System.Windows.Forms.Label();
+            this.SourceDirText = new System.Windows.Forms.TextBox();
+            this.SourceDirButton = new System.Windows.Forms.Button();
+            this.OutputDirLabel = new System.Windows.Forms.Label();
+            this.OutputDirText = new System.Windows.Forms.TextBox();
+            this.OutputDirButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StatusText = new System.Windows.Forms.Label();
+            this.FormatLabel = new System.Windows.Forms.Label();
+            this.ExtensionInput = new System.Windows.Forms.TextBox();
+            this.AreNumbered = new System.Windows.Forms.CheckBox();
+            this.PrefixText = new System.Windows.Forms.TextBox();
+            this.PrefixLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SourceDirLabel
+            // 
+            this.SourceDirLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SourceDirLabel.Location = new System.Drawing.Point(12, 15);
+            this.SourceDirLabel.Name = "SourceDirLabel";
+            this.SourceDirLabel.Size = new System.Drawing.Size(81, 19);
+            this.SourceDirLabel.TabIndex = 0;
+            this.SourceDirLabel.Text = "Source Dir:";
+            this.SourceDirLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // SourceDirText
+            // 
+            this.SourceDirText.Location = new System.Drawing.Point(85, 12);
+            this.SourceDirText.Name = "SourceDirText";
+            this.SourceDirText.ReadOnly = true;
+            this.SourceDirText.Size = new System.Drawing.Size(260, 20);
+            this.SourceDirText.TabIndex = 1;
+            // 
+            // SourceDirButton
+            // 
+            this.SourceDirButton.Location = new System.Drawing.Point(351, 12);
+            this.SourceDirButton.Name = "SourceDirButton";
+            this.SourceDirButton.Size = new System.Drawing.Size(75, 20);
+            this.SourceDirButton.TabIndex = 2;
+            this.SourceDirButton.Text = "Select";
+            this.SourceDirButton.UseVisualStyleBackColor = true;
+            this.SourceDirButton.Click += new System.EventHandler(this.SourceDirButton_Click);
+            // 
+            // OutputDirLabel
+            // 
+            this.OutputDirLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.OutputDirLabel.Location = new System.Drawing.Point(12, 40);
+            this.OutputDirLabel.Name = "OutputDirLabel";
+            this.OutputDirLabel.Size = new System.Drawing.Size(81, 19);
+            this.OutputDirLabel.TabIndex = 3;
+            this.OutputDirLabel.Text = "Output Dir:";
+            this.OutputDirLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // OutputDirText
+            // 
+            this.OutputDirText.Location = new System.Drawing.Point(85, 37);
+            this.OutputDirText.Name = "OutputDirText";
+            this.OutputDirText.ReadOnly = true;
+            this.OutputDirText.Size = new System.Drawing.Size(260, 20);
+            this.OutputDirText.TabIndex = 4;
+            // 
+            // OutputDirButton
+            // 
+            this.OutputDirButton.Location = new System.Drawing.Point(351, 37);
+            this.OutputDirButton.Name = "OutputDirButton";
+            this.OutputDirButton.Size = new System.Drawing.Size(75, 20);
+            this.OutputDirButton.TabIndex = 5;
+            this.OutputDirButton.Text = "Select";
+            this.OutputDirButton.UseVisualStyleBackColor = true;
+            this.OutputDirButton.Click += new System.EventHandler(this.OutputDirButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Enabled = false;
+            this.StartButton.Location = new System.Drawing.Point(15, 145);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(411, 88);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StatusText
+            // 
+            this.StatusText.Location = new System.Drawing.Point(15, 236);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(411, 37);
+            this.StatusText.TabIndex = 7;
+            this.StatusText.Text = "Status";
+            this.StatusText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.StatusText.Visible = false;
+            // 
+            // FormatLabel
+            // 
+            this.FormatLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.FormatLabel.Location = new System.Drawing.Point(12, 64);
+            this.FormatLabel.Name = "FormatLabel";
+            this.FormatLabel.Size = new System.Drawing.Size(78, 19);
+            this.FormatLabel.TabIndex = 8;
+            this.FormatLabel.Text = "Extension: ";
+            this.FormatLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ExtensionInput
+            // 
+            this.ExtensionInput.Location = new System.Drawing.Point(85, 63);
+            this.ExtensionInput.Name = "ExtensionInput";
+            this.ExtensionInput.Size = new System.Drawing.Size(66, 20);
+            this.ExtensionInput.TabIndex = 9;
+            this.ExtensionInput.Text = "png";
+            this.ExtensionInput.TextChanged += new System.EventHandler(this.ExtensionInput_TextChanged);
+            // 
+            // AreNumbered
+            // 
+            this.AreNumbered.AutoSize = true;
+            this.AreNumbered.Location = new System.Drawing.Point(272, 66);
+            this.AreNumbered.Name = "AreNumbered";
+            this.AreNumbered.Size = new System.Drawing.Size(154, 17);
+            this.AreNumbered.TabIndex = 10;
+            this.AreNumbered.Text = "Source Files are Numbered";
+            this.AreNumbered.UseVisualStyleBackColor = true;
+            // 
+            // PrefixText
+            // 
+            this.PrefixText.Location = new System.Drawing.Point(85, 89);
+            this.PrefixText.Name = "PrefixText";
+            this.PrefixText.Size = new System.Drawing.Size(144, 20);
+            this.PrefixText.TabIndex = 12;
+            // 
+            // PrefixLabel
+            // 
+            this.PrefixLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PrefixLabel.Location = new System.Drawing.Point(12, 90);
+            this.PrefixLabel.Name = "PrefixLabel";
+            this.PrefixLabel.Size = new System.Drawing.Size(78, 19);
+            this.PrefixLabel.TabIndex = 11;
+            this.PrefixLabel.Text = "Extension: ";
+            this.PrefixLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ActorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 294);
+            this.Controls.Add(this.PrefixText);
+            this.Controls.Add(this.PrefixLabel);
+            this.Controls.Add(this.AreNumbered);
+            this.Controls.Add(this.ExtensionInput);
+            this.Controls.Add(this.FormatLabel);
+            this.Controls.Add(this.StatusText);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.OutputDirButton);
+            this.Controls.Add(this.OutputDirText);
+            this.Controls.Add(this.OutputDirLabel);
+            this.Controls.Add(this.SourceDirButton);
+            this.Controls.Add(this.SourceDirText);
+            this.Controls.Add(this.SourceDirLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ActorForm";
+            this.Text = "RPG Maker Faces Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label SourceDirLabel;
+        private System.Windows.Forms.TextBox SourceDirText;
+        private System.Windows.Forms.Button SourceDirButton;
+        private Ookii.Dialogs.Wpf.VistaFolderBrowserDialog SourceDirDialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+        private string SelectedInputDir;
+        private System.Windows.Forms.Label OutputDirLabel;
+        private System.Windows.Forms.TextBox OutputDirText;
+        private System.Windows.Forms.Button OutputDirButton;
+        private Ookii.Dialogs.Wpf.VistaFolderBrowserDialog OutputDirDialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+        private string SelectedOutputDir;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label StatusText;
+        private System.Windows.Forms.Label FormatLabel;
+        private System.Windows.Forms.TextBox ExtensionInput;
+        private System.Windows.Forms.CheckBox AreNumbered;
+        private System.Windows.Forms.TextBox PrefixText;
+        private System.Windows.Forms.Label PrefixLabel;
+    }
+}
+
---
+++ RPGMakerActorGenerator/ActorForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPGMakerActorGenerator
+{
+    public partial class ActorForm : Form
+    {
+        public ActorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SourceDirButton_Click(object sender, EventArgs e)
+        {
+            bool? v = SourceDirDialog.ShowDialog();
+            if (v != null && (bool)v)
+            {
+                SelectedInputDir = SourceDirDialog.SelectedPath;
+            } else
+            {
+                SelectedInputDir = "";
+            }
+            SourceDirText.Text = SelectedInputDir;
+            UpdateStartButton();
+        }
+
+        private void OutputDirButton_Click(object sender, EventArgs e)
+        {
+            bool? v = OutputDirDialog.ShowDialog();
+            if (v != null && (bool)v)
+            {
+                SelectedOutputDir = OutputDirDialog.SelectedPath;
+            } else
+            {
+                SelectedOutputDir = "";
+            }
+            OutputDirText.Text = SelectedOutputDir;
+            UpdateStartButton();
+        }
+
+        private void UpdateStartButton()
+        {
+            StartButton.Enabled = !string.IsNullOrWhiteSpace(SelectedInputDir) && !string.IsNullOrWhiteSpace(SelectedOutputDir) &&
+                !string.IsNullOrWhiteSpace(ExtensionInput.Text);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StatusText.Visible = true;
+            StatusText.Text = "";
+
+            string[] images = Directory.GetFiles(SelectedInputDir, "*." + ExtensionInput.Text);
+            int totalImages = (int)Math.Ceiling(images.Length / 8f);
+
+            if (AreNumbered.Checked)
+            {
+                Array.Sort(images, (a, b) =>
+                {
+                    a = Path.GetFileNameWithoutExtension(Path.GetFileName(a));
+                    b = Path.GetFileNameWithoutExtension(Path.GetFileName(b));
+                    int aInt = int.Parse(a);
+                    int bInt = int.Parse(b);
+
+                    return aInt - bInt;
+                });
+            }
+
+            for (int i = 0; i < totalImages; i++)
+            {
+                Bitmap newImg = new Bitmap(144 * 4, 144 * 2);
+                Graphics g = Graphics.FromImage(newImg);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+
+                var arr = images.Skip(i * 8).Take(8);
+                int index = 0;
+
+                foreach (string value in arr)
+                {
+                    int xPos = index < 4 ? index : index - 4;
+                    int yPos = index < 4 ? 0 : 1;
+                    Image face = ResizeBitmap(Image.FromFile(value));
+
+                    g.DrawImage(face, xPos * 144, yPos * 144);
+                    index++;
+
+                    face.Dispose();
+                }
+
+                newImg.Save(SelectedOutputDir + "\\" + PrefixText.Text + (i + 1).ToString() + ".png");
+                newImg.Dispose();
+                g.Dispose();
+            }
+
+            StatusText.Text = "Complete!";
+        }
+
+        private Bitmap ResizeBitmap(Image source)
+        {
+            Bitmap final = new Bitmap(144, 144);
+            using (Graphics g = Graphics.FromImage(final))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImage(source, 0, 0, 145, 145);
+            }
+            source.Dispose();
+            return final;
+        }
+
+        private void ExtensionInput_TextChanged(object sender, EventArgs e)
+        {
+            UpdateStartButton();
+        }
+    }
+}
